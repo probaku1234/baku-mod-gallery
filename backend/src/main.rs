497539e46ea2ea7eb42c0a33261fe66b081c73e6
Yun Seob Lee <fhzotxldj@gmail.com>
2024-01-19 22:02:51 +0900

    Auto stash before merge of "main" and "origin/main"

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,11 +1,15 @@
 use axum::{routing::get, Router};
+use mongodb;
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
 #[shuttle_runtime::main]
-async fn main() -> shuttle_axum::ShuttleAxum {
+async fn main(
+    #[shuttle_shared_db::MongoDb] mongo: mongodb::Database,
+    #[shuttle_secrets::Secrets] secret_store: shuttle_secrets::SecretStore
+) -> shuttle_axum::ShuttleAxum {
     let router = Router::new().route("/", get(hello_world));
 
     Ok(router.into())

ef2a8061a3f48213168b31aa92fda56ad9cc617a
Yun Seob Lee <fhzotxldj@gmail.com>
2024-01-21 02:19:37 +0900

    implement get_post_by_id and test code

--- backend/src/posts.rs
+++ backend/src/posts.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 use crate::AppState;
 use axum::response::IntoResponse;
 use axum::{
@@ -6,7 +8,8 @@ use axum::{
     Json,
 };
 use futures::stream::TryStreamExt;
-use mongodb::bson::{Bson, DateTime};
+use mongodb::bson::oid::ObjectId;
+use mongodb::bson::{bson, Bson, DateTime};
 use mongodb::{bson::doc, options::FindOptions};
 use serde::{Deserialize, Serialize};
 use tracing::{debug, error, info};
@@ -25,11 +28,11 @@ pub struct Post {
 pub struct NewPostRequest {
     title: String,
     images_url: Vec<String>,
-    file_url: String
+    file_url: String,
 }
 
 pub async fn get_all_posts(
-    State(state): State<AppState>
+    State(state): State<AppState>,
 ) -> Result<Json<Vec<Post>>, impl IntoResponse> {
     let typed_collection = &state.mongo.collection::<Post>("posts");
 
@@ -40,6 +43,45 @@ pub async fn get_all_posts(
         Ok(cursor) => {
             let posts = cursor.try_collect().await.unwrap_or_else(|_| vec![]);
             Ok(Json(posts))
+        }
+        Err(err) => {
+            let error_message = err.to_string();
+            error!("{}", error_message.clone());
+            Err((StatusCode::INTERNAL_SERVER_ERROR, err.to_string()).into_response())
+        }
+    }
+}
+
+pub async fn get_post_by_id(
+    State(state): State<AppState>,
+    Path(id): Path<String>,
+) -> Result<Json<Post>, impl IntoResponse> {
+    let typed_collection = &state.mongo.collection::<Post>("posts");
+
+    let target_post_object_id_result = ObjectId::from_str(&id);
+
+    if target_post_object_id_result.is_err() {
+        let error_message = target_post_object_id_result.unwrap_err().to_string();
+        error!(error_message);
+
+        return Err((StatusCode::BAD_REQUEST, error_message).into_response());
+    }
+
+    let filter = doc! {
+        "_id": target_post_object_id_result.unwrap()
+    };
+
+    match typed_collection.find_one(filter, None).await {
+        Ok(result) => match result {
+            Some(post) => Ok(Json(post)),
+            None => {
+                error!("The post with id: {} not found!", id);
+                Err((
+                    StatusCode::NOT_FOUND,
+                    format!("The post with id: {} not found!", id),
+                )
+                    .into_response())
+            }
         },
         Err(err) => {
             let error_message = err.to_string();
@@ -51,7 +93,7 @@ pub async fn get_all_posts(
 
 pub async fn create_new_post(
     State(state): State<AppState>,
-    Json(req): Json<NewPostRequest>
+    Json(req): Json<NewPostRequest>,
 ) -> Result<Json<Bson>, impl IntoResponse> {
     let typed_collection = &state.mongo.collection::<Post>("posts");
 
@@ -67,7 +109,7 @@ pub async fn create_new_post(
         Ok(result) => {
             info!("New Post Created");
             Ok(Json(result.inserted_id))
-        },
+        }
         Err(err) => {
             let error_message = err.to_string();
             error!("{}", error_message.clone());
@@ -82,12 +124,14 @@ use test_env_helpers::*;
 #[before_all]
 #[cfg(test)]
 mod tests {
-    use crate::test_util::test_util::{generate_port_number, get_mongo_image, populate_test_data, get_db_connection_uri};
-    use testcontainers::clients;
-    use mongodb::Client;
-    use crate::AppState;
-    use serde_json::{json, Value};
     use super::*;
+    use crate::test_util::test_util::{
+        generate_port_number, get_db_connection_uri, get_mongo_image, populate_test_data,
+    };
+    use crate::AppState;
+    use mongodb::Client;
+    use serde_json::{json, Value};
+    use testcontainers::clients;
 
     async fn before_all() {
         // let docker = clients::Cli::default();
@@ -108,9 +152,7 @@ mod tests {
 
         let test_db = client.database("test_db");
 
-        let state = AppState {
-            mongo: test_db
-        };
+        let state = AppState { mongo: test_db };
 
         let result = get_all_posts(State(state)).await;
 
@@ -120,7 +162,30 @@ mod tests {
         // assert_eq!(result.unwrap()., Json(vec![]));
     }
 
-    #[tokio::test]
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_get_post_by_id_invalid_id() {
+        let docker = clients::Cli::default();
+        let port = generate_port_number();
+        let mongo_img = get_mongo_image(&port);
+        let _c = docker.run(mongo_img);
+        populate_test_data(&port);
+        let uri = get_db_connection_uri(&port);
+        let client = Client::with_uri_str(uri).await.unwrap();
+
+        let test_db = client.database("test_db");
+
+        let state = AppState { mongo: test_db };
+
+        let result = get_post_by_id(State(state), Path("aaaa".to_string())).await;
+
+        assert!(result.is_err());
+
+        let response = result.unwrap_err();
+
+        assert_eq!(response.into_response().status(), StatusCode::BAD_REQUEST);
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
     async fn test_create_new_posts() {
         let docker = clients::Cli::default();
         let port = generate_port_number();
@@ -132,9 +197,7 @@ mod tests {
 
         let test_db = client.database("test_db");
 
-        let state = AppState {
-            mongo: test_db
-        };
+        let state = AppState { mongo: test_db };
         let new_post_request = NewPostRequest {
             title: "aa".to_string(),
             images_url: vec![],
--- backend/src/router.rs
+++ backend/src/router.rs
@@ -1,4 +1,4 @@
-use crate::posts::{create_new_post, get_all_posts};
+use crate::posts::{create_new_post, get_all_posts, get_post_by_id};
 use crate::AppState;
 use axum::{
     http::{self},
@@ -10,6 +10,7 @@ use axum::{
 pub fn create_api_router(state: AppState) -> Router {
     let posts_router = Router::new()
         .route("/", get(get_all_posts))
+        .route("/:id", post(get_post_by_id))
         .route("/create", post(create_new_post));
 
     Router::new()

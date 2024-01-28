00822a17c05554bcb68479553f9d173d729cbe76
Yun Seob Lee <fhzotxldj@gmail.com>
2024-01-28 22:04:01 +0900

    define types for next-auth and post

---
+++ frontend/types/global.d.ts
@@ -0,0 +1,19 @@
+export {};
+
+declare global {
+  interface IPost {
+    _id: {
+      $oid: string;
+    };
+    title: string;
+    images_url: string[];
+    file_url: string;
+    created_at: Date;
+    updated_at: Date;
+  }
+
+  interface IJWTPayload {
+    name: string;
+    role: string;
+  }
+}

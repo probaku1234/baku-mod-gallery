67d94cc908c4dee5ab3244ec26381e353b11f7fe
Yun Seob Lee <fhzotxldj@gmail.com>
2024-02-03 14:10:54 +0900

    add logging to middleware

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -10,15 +10,16 @@ export async function middleware(req: NextRequest, event: NextFetchEvent) {
   console.log("middleware");
   // 2022/08/13 - 로그인/회원가입 접근 제한 - by 1-blue
   if (pathname.startsWith("/admin")) {
-    console.log(token);
     if (!token) {
+      console.log("not signed, redirect to login page");
       return NextResponse.redirect(new URL("/login", req.url));
     } else if (token.role !== "admin") {
+      console.log("not admin, redirect to main page");
       return NextResponse.redirect(new URL("/", req.url));
     }
   }
 }
 
 export const config = {
-  matcher: ["/admin",],
+  matcher: ["/admin"],
 };
